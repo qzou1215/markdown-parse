9f7bfb7a3c9f03ee931b1a7ca449500bbb86a92d
qzou1215 <zouqt.christine2018@gdhfi.com>
2022-01-27 19:26:20 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,21 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void testGetLinks() throws IOException{
+        String[] args = {"/Users/zouqingtong/Documents/GitHub/markdown-parse/test-file.md"};
+        Path fileName = Path.of(args[0]);
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals("[https://something.com, some-page.html]", links.toString());
     }
 }
