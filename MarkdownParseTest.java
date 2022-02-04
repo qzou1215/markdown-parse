8ebcbde02c8a57ed983e81236966788b5b8ebeda
qzou1215 <zouqt.christine2018@gdhfi.com>
2022-02-03 18:33:47 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -11,11 +11,28 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);
     }
     @Test
-    public void testGetLinks() throws IOException{
+    public void testFile() throws IOException{
         String[] args = {"/Users/zouqingtong/Documents/GitHub/markdown-parse/test-file.md"};
         Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         assertEquals("[https://something.com, some-page.html]", links.toString());
     }
+    @Test
+    public void testnewFile() throws IOException{
+        String[] args = {"/Users/zouqingtong/Documents/GitHub/markdown-parse/newFile.md"};
+        Path fileName = Path.of(args[0]);
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals("[No link found.]", links.toString());
+    }
+    @Test
+    public void testanotherNewFile() throws IOException{
+        String[] args = {"anotherNewFile"};
+        Path fileName = Path.of(args[0]);
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        assertEquals("[No link in the parenthesis.]", links.toString());
+    }
+
 }
